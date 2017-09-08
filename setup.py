67f3655db2a7cb1bbd19546b710b8d49c752fbae
Juan Manuel Torres <juant@mindtouch.com>
2017-09-07 17:44:21 -0700

    Adding py36 requirements, update information

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
+import sys
 from setuptools import setup
 
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
 setup(
     name='cfn-response',
-    version='0.0.4',
-    url='http://github.com/jorgebastida/cfn-response',
+    version='0.0.5',
+    url='http://github.com/onema/cfn-response',
     license='BSD',
-    author='Jorge Bastida',
-    author_email='me@jorgebastida.com',
+    author='Jorge Bastida, Juan Manuel Torres',
+    author_email='me@jorgebastida.com, kinojman@gmail.com',
     description="""cfn-response is a micro package which exposes some
     helpers to talk with CloudFormation from within python Lambda functions.
     This package is intentionally small in order to be embedded lambda functions.
@@ -15,12 +18,14 @@ setup(
     keywords="aws lambda cloudformation custom resources",
     py_modules=["cfnresponse"],
     platforms='any',
-    install_requires=[],
-    test_suite='tests',
+    install_requires=[
+        'requests>=2.18.4',
+    ],
+    test_suite='nose.collector',
+    tests_require=['nose', 'coverage', 'mock'],
     classifiers=[
-        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 3',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Beta',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
